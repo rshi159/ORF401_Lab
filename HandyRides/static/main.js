9a25ff9fe87447beedba41d0bc5233b477ed3fe2
rshi159 <rshi0611@gmail.com>
2021-02-20 13:35:00 -0500

    completed lab2 question 3 a and b

--- HandyRides/static/main.js
+++ HandyRides/static/main.js
@@ -17,10 +17,40 @@ function setCookie(c_name,value,exdays) {
    document.cookie=c_name + "=" + c_value;
 }
 
+// checks if user visited page before. if not, set cookie
+// and redirect to splash page
+function checkCookie(c_name, value, exdays) {
+   if(getCookie(c_name) == null){
+      console.log("New Visitor");
+      setCookie(c_name, value, exdays);
+      console.log("Saved cookie. Redirecting...");
+      window.location.replace("http://127.0.0.1:8000/");
+   }
+   console.log("exited check cookie function");
+}
+
 function checkForm(form){
-   var search = form.children[1].value;
-   if (search.filter("elon") & search.filter("musk")) {
-      alert("He's not here!!!")
+   var search_city = form.children[1].value;
+   var search_state = form.children[3].value;
+   console.log(search_city);
+   console.log(search_state);
+
+   var elon = "elon musk";
+
+   if(search_city.toLowerCase().includes(elon))
+      alert("He's not here!!!");
+
+   // //https://stackoverflow.com/questions/10232366/how-to-check-if-a-variable-is-null-or-empty-string-or-all-whitespace-in-javascri
+   if (search_city == null | search_city.match(/^ *$/) !== null)
+   {
+      console.log("no input in search city");
+      return false;
+   }
+   if (search_state == null | search_state.match(/^ *$/) !== null)
+   {
+      console.log("no input in search state");
+      return false;
    }
 
+   return true;
 }
