241f0a7f3f10609b2d22bb10571dcf608c0e5b47
rshi159 <rshi0611@gmail.com>
2021-03-01 11:40:03 -0500

    Actually fixed phone number

--- HandyRides/add_ride/models.py
+++ HandyRides/add_ride/models.py
@@ -1,6 +1,19 @@
 from django.db import models
-
+from django.core.validators import RegexValidator
+import re
 # Create your models here.
+
+# https://stackoverflow.com/questions/59480134/how-to-store-a-phone-number-in-django-model
+# error message when a wrong format entered
+phone_message = 'Phone number must be entered in the format: 05999999999' 
+
+ # your desired format 
+phone_regex = RegexValidator(
+    # regex=r^[-+]?[0-9]+$,
+    regex = re.compile(r'\d+'),
+    message=phone_message
+)
+
 
 
 class Person(models.Model):
@@ -8,7 +21,10 @@ class Person(models.Model):
   last_name = models.CharField(max_length=64)
   email = models.CharField(max_length=64)
   rating = models.IntegerField()
-  phone_number = models.IntegerField()
+  # phone_number = models.IntegerField()
+  # finally, your phone number field
+  # phone_number = models.CharField(validators=[phone_regex], max_length=60)
+  phone_number = models.CharField(max_length=60)
   origination_address = models.CharField(max_length=64)
   origination_city = models.CharField(max_length=64)
   origination_state = models.CharField(max_length=2)
